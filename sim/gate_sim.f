3d8627e69bf1c0d6ac56c9216257872b0f6103cf
yaohsiao <yaohsiaopid@gmail.com>
2020-01-12 20:56:22 +0800

    update wroker testbench for gate sim

---
+++ sim/gate_sim.f
@@ -0,0 +1,9 @@
+test_worker_gate.v
+./sram_model/vid_sram_16x256b.v
+./sram_model/dist_sram_NxNb.v     
+./sram_model/next_sram_256x4b.v      
+./sram_model/loc_sram_16x1280b.v  
+./sram_model/proposal_sram_16x128b.v
+../syn_8/netlist/worker_syn.v
+-v /usr/cadtool/cad/synopsys/SAED32_EDK/lib/stdcell_hvt/verilog/saed32nm_hvt.v
++access+r
